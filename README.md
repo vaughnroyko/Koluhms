7396a75950da8d84739a300f4419f7325cd2a003
Vaughn Royko <drathy@gmail.com>
2015-08-04 14:28:22 -0700

    Initial (version 1.0) release.

--- README.md
+++ README.md
@@ -1,2 +1,151 @@
-# Koluhms
+# Koluhms v1.0
 An automated column [kol-uh m] solution with support for height balancing, responsive styles and more. Configurable right in your HTML and CSS.
+
+Requires
+--------------
+
+jQuery.
+
+Support
+--------------
+
+Most things? It appears to work in IE9.
+
+Usage
+--------------
+
+Basic example using lists with sub lists:
+```html
+<ul data-columns="3" data-selector="li" data-identifier="column" data-column-element="ul">
+    <li>Test #1</li>
+    <li>Test #2</li>
+    <li>Test #3</li>
+    <li>Test #4
+        <ul>
+            <li>Sub Test #1</li>
+            <li>Sub Test #2</li>
+            <li>Sub Test #3</li>
+            <li>Sub Test #4</li>
+        </ul>
+    </li>
+    <li>Test #5</li>
+    <li>Test #6</li>
+    <li>Test #7
+        <ul>
+            <li>Sub Test #5</li>
+            <li>Sub Test #6</li>
+            <li>Sub Test #7</li>
+            <li>Sub Test #8</li>
+            <li>Sub Test #9</li>
+            <li>Sub Test #10</li>
+            <li>Sub Test #11</li>
+            <li>Sub Test #12</li>
+            <li>Sub Test #13</li>
+        </ul>
+    </li>
+    <li>Test #8
+        <ul>
+            <li>Sub Test #14</li>
+            <li>Sub Test #15</li>
+        </ul>
+    </li>
+</ul>
+```
+
+```css
+.column {
+    float: left;
+    width: 33%;
+}
+```
+
+Same example using balance property:
+```html
+<ul data-columns="3" data-selector="li" data-identifier="column-balance" data-column-element="ul" data-balance="true">
+    <li>Test #1</li>
+    <li>Test #2</li>
+    <li>Test #3</li>
+    <li>Test #4
+        <ul>
+            <li>Sub Test #1</li>
+            <li>Sub Test #2</li>
+            <li>Sub Test #3</li>
+            <li>Sub Test #4</li>
+        </ul>
+    </li>
+    <li>Test #5</li>
+    <li>Test #6</li>
+    <li>Test #7
+        <ul>
+            <li>Sub Test #5</li>
+            <li>Sub Test #6</li>
+            <li>Sub Test #7</li>
+            <li>Sub Test #8</li>
+            <li>Sub Test #9</li>
+            <li>Sub Test #10</li>
+            <li>Sub Test #11</li>
+            <li>Sub Test #12</li>
+            <li>Sub Test #13</li>
+        </ul>
+    </li>
+    <li>Test #8
+        <ul>
+            <li>Sub Test #14</li>
+            <li>Sub Test #15</li>
+        </ul>
+    </li>
+</ul>
+```
+
+```css
+.column-balance {
+    float: left;
+    width: 33%;
+}
+```
+
+Example using responsive media queries for alternate column counts:
+```html
+<div data-columns="2,3,4" data-breaks="480,768,1140" data-selector="p" data-identifier="column-responsive" data-column-element="div" data-balance="true">
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce semper libero a orci accumsan egestas.</p>
+    <p>Integer sit amet odio quis neque viverra ultricies quis vitae diam.</p>
+    <p>Nam quis ex ac nulla convallis fringilla. Phasellus eleifend ac leo id mattis.</p>
+    <p>Integer tincidunt magna eget cursus sodales. Aliquam vel massa velit.</p>
+</div>
+```
+
+```css
+@media only screen and (min-width: 480px) {
+    .column-responsive {
+        float: left;
+        width: 50%;
+    }
+}
+
+@media only screen and (min-width: 768px) {
+    .column-responsive {
+        width: 33%;
+    }
+}
+
+@media only screen and (min-width: 1140px) {
+    .column-responsive {
+        width: 25%;
+    }
+}
+```
+
+Notes
+--------------
+
+The new columns that are created will be appended to the parent element of the element you attach data-columns to. There is very little error checking on set parameters currently. The only optional data parameters are data-balance and data-breaks. data-columns and data-breaks are expected to be numbers, or a list of numbers, using a "," to separate. Make sure they have the same amount of numbers in them. Make sure they appear in an order smallest to largest.
+
+Demo
+--------------
+
+[Demo Link](http://htmlpreview.github.io/?https://github.com/vaughnroyko/Koluhms/blob/master/demo.html)
+
+License
+--------------
+
+Koluhms is licensed under the [MIT License](https://github.com/vaughnroyko/Koluhms/blob/master/LICENSE).
---
+++ koluhms.js
@@ -0,0 +1,128 @@
+/*!
+ * Koluhms
+ * An automated column [kol-uh m] solution with support for height balancing, responsive styles and more. Configurable right in your HTML and CSS.
+ * Version: 1.0 (August 4th, 2015)
+ * requires jQuery
+ */
+
+var koluhms;
+
+$(function () {
+
+    var Koluhms = function () {
+
+        this.makeColumns = function() {
+
+            $('[data-columns]').each(function () {
+
+                var columns = $(this).data('columns');
+                if (columns) {
+                    columns = columns.toString().split(',');
+                }
+                var columnSelection = 0;
+                var breaks = $(this).data('breaks');
+                if (breaks) {
+                    breaks = breaks.toString().split(',');
+                    // Find out which breakpoint we are making this pass
+                    var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+                    for(var i = 0; i < breaks.length; i++) {
+                        if (width >= parseInt(breaks[i], 10)) {
+                            columnSelection = parseInt(columns[i], 10);
+                        }
+                    }
+                } else {
+                    columnSelection = parseInt(columns[0]);
+                }
+
+                columns = columnSelection;
+
+                var selector = $(this).data('selector');
+                var columnElement = $(this).data('column-element');
+                var balance = $(this).data('balance');
+                var columnItems = $(this).children(selector);
+                var container = $(this).parent();
+                var identifier = $(this).data('identifier');
+                // Check if the columns were already created
+                if (container.find('.' + identifier).length == columns) {
+                    return;
+                }
+
+                // Remove older ones that have been created
+                container.find('.' + identifier).remove();
+
+                // If there's 0 columns set at any point, revert everything
+                if (columns === 0) {
+                    $(this).show();
+                    return;
+                }
+
+                for (var i = 0; i < columns; i++) {
+                    container.append('<' + columnElement + ' class="' + identifier + '"></ul>');
+                }
+
+                var columnCount = 0;
+                columnItems.each(function () {
+                    var newHTML = $($(this)[0].outerHTML);
+                    container.find('.' + identifier + ':eq(' + columnCount + ')').append(newHTML);
+                    columnCount++;
+                    if (columnCount >= columns) {
+                        columnCount = 0;
+                    }
+                });
+
+                var getAndSortHeights = function () {
+                    var columnHeights = [];
+                    for (var i = 0; i < columns; i++) {
+                        columnHeights[i] = {
+                            eq: i,
+                            height: container.find('.' + identifier + ':eq(' + i + ')').outerHeight()
+                        };
+                    }
+
+                    // Sort the columns from smallest to biggest
+                    columnHeights.sort(function (a, b) {
+                        return a.height - b.height;
+                    });
+
+                    return columnHeights;
+                };
+
+                var sortColumns = function () {
+                    var newSmallestColumn = container.find('.' + identifier + ':eq(' + columnHeights[0].eq + ')');
+                    $(this).detach().appendTo(newSmallestColumn);
+                    if (newSmallestColumn.outerHeight() >= newBiggestColumn.outerHeight()) {
+                        // Move it back if it's too big
+                        newSmallestColumn.children(selector + ":last").detach().appendTo(newBiggestColumn);
+                    } else {
+                        // Successful move, recalculate the heights and start over
+                        columnHeights = getAndSortHeights();
+                        tries = columnHeights.length;
+                        return false;
+                    }
+                };
+
+                // Are we trying to balance the height of the columns?
+                if (balance) {
+
+                    var columnHeights = getAndSortHeights();
+
+                    // Attempt to balance
+                    for (var tries = columnHeights.length - 1; tries > 0; tries--) {
+                        var newBiggestColumn = container.find('.' + identifier + ':eq(' + (columnHeights[columns - tries].eq) + ')');
+                        newBiggestColumn.children(selector).each(sortColumns);
+                    }
+
+                }
+
+                $(this).hide();
+            });
+        };
+
+    };
+
+    // Init
+    koluhms = new Koluhms();
+    $(window).on('resize', koluhms.makeColumns);
+    koluhms.makeColumns();
+
+});
